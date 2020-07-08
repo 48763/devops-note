189ad34f721dda2cf3c9879cfbc4e1dddcd85f50
48763 <future.starshine@gmail.com>
2020-07-08 23:03:40 +0800

    Update reload-kibana.sh

--- troubleshooting/reload-kibana.sh
+++ troubleshooting/reload-kibana.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
+PLUGIN_PATH="/usr/share/elasticsearch/bin/elasticsearch-plugin"
+GCP_DOMAIN=".gcp.silkrode.com.tw"
+
 echo > fail.txt
 for proj in `gcloud projects list --format="[no-heading](PROJECT_ID)"`
 do
 	for pc in `gcloud compute instances list --format="[no-heading](NAME)" --filter="NAME:-es-" --project $proj`
 	do
-		ping -c 1 prod-bms-es-tw-d01  > /dev/null 2>&1
+		echo "Try into $pc of $proj".
+		
+		ping -c 1 -w 1 $pc$GCP_DOMAIN > /dev/null 2>&1
+
 		if [ $? -eq 0 ]; then
-			ssh -t ayang@prod-bms-es-tw-d01 "
+			ssh -t $pc$GCP_DOMAIN "
+				echo \"Already on $pc.\"
 				sudo systemctl daemon-reload;
 				sudo systemctl restart elasticsearch > /dev/null 2>&1;
 				if [ \$? -ne 0 ]; then
-					echo $pc of $proj elasticsearch restart fail.
+					echo $pc of $proj check elasticsearch restart fail.
 				fi
-				sudo systemctl restart kibana" >> fail.txt
+			" >> fail.txt
+			
+			echo "done!"
+		else 
+			echo "$pc of $proj can't touch.". >> fail.txt
+			echo "fail!"
 		fi
 	done
 done
